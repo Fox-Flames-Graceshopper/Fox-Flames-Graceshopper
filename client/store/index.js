3f26b11085244c6926b4b357402de4daadf7b799
John M <mccrayjrob@gmail.com>
2021-03-02 17:44:00 -0500

    Feat: Single Product View

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,10 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import singleProduct from './singleProduct'
 import user from './user'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, singleProduct})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
