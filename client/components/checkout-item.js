be1ff7f750c72de1bd8ae91edcf31f49cc006dad
John Vazquez <johnnyvcoding@gmail.com>
2021-03-03 16:37:46 -0600

    feature: added css to checkout

--- client/components/checkout-item.js
+++ client/components/checkout-item.js
@@ -1,18 +1,36 @@
 import React from 'react'
 
-function CheckoutItem(props) {
-  return (
-    <div className="chk-item-cont">
-      <div className="item-img-cont">
-        <img src={props.imageUrl} />
-      </div>
+class CheckoutItem extends React.Component {
+  constructor() {
+    super()
 
-      <div>
-        <h2>{props.name}</h2>
-        <span>{props.price}</span>
+    this.state = {
+      quantity: 1
+    }
+  }
+
+  handleChange(event) {}
+
+  render() {
+    return (
+      <div className="chk-item-cont">
+        <div className="item-img-cont">
+          <img className="cart-img" src={this.props.imageUrl} />
+        </div>
+
+        <div className="item-cart-info">
+          <h2 className="item-cart-name">{this.props.name}</h2>
+          <span id="item-cart-price">${this.props.price}</span>
+          <div>
+            {/* <div>
+              <input type="text" />
+            </div> */}
+            <button type="submit">Delete</button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default CheckoutItem
