addf74b9378805afcf6ca84e44bf92ef9592a822
Tommy Liu <t.liu625@gmail.com>
2021-03-10 10:39:32 -0500

    fixed: jewelry spelling and centered search bar

--- script/seed.js
+++ script/seed.js
@@ -43,7 +43,7 @@ const productData = [
     price: 695,
     description:
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-    category: 'jewelery',
+    category: 'jewelry',
     imageUrl: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg'
   },
   {
@@ -51,7 +51,7 @@ const productData = [
     price: 168,
     description:
       'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.',
-    category: 'jewelery',
+    category: 'jewelry',
     imageUrl: 'https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg'
   },
   {
@@ -59,7 +59,7 @@ const productData = [
     price: 9.99,
     description:
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
-    category: 'jewelery',
+    category: 'jewelry',
     imageUrl: 'https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg'
   },
   {
@@ -67,7 +67,7 @@ const productData = [
     price: 10.99,
     description:
       'Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel',
-    category: 'jewelery',
+    category: 'jewelry',
     imageUrl: 'https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg'
   },
   {
@@ -1619,13 +1619,13 @@ const newWomenClothingData = [
   }
 ]
 
-const newJeweleryData = [
+const newJewelryData = [
   {
     name: 'Open Graduated Size Hoop Earring Set 3ct - Wild Fable&#8482; Gold',
     price: 6,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_c0b1aa19-e2a1-484b-beb0-49e148218f01',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Three pairs of open hoop earrings in different sizes. Open construction at the back of the hoop offers easy, comfortable wear. Gold finish brings added shine.',
     reviews: [
@@ -1655,7 +1655,7 @@ const newJeweleryData = [
     price: 4.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_a3552437-7f5e-46ab-ba60-42b28fb77768',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Regular use of Connoisseurs Precious Jewelry Cleaner will bring out the brilliance of your gold, platinum, diamonds & precious stone jewelry. Special polymers in our jewelry cleaning formula help to reduce the appearance of tiny scratches in settings. Connoisseurs Precious jewelry cleaner is a good way to clean most metals used in jewelry today.',
     reviews: [
@@ -1684,7 +1684,7 @@ const newJeweleryData = [
     price: 7.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_455c3e3e-ac92-4211-8f1a-5a0e7b61f643',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Get a hoop for every occasion with these Small Hoop Earrings from A New Day™. Hinged huggies in three different diameters sport a sleek, squared design that hugs the earlobe for a chic finish. Accent your go-to jeans and a tee look with the tiny pair, or bring on the big hoops to glam up a workday ensemble.',
     reviews: [
@@ -1715,7 +1715,7 @@ const newJeweleryData = [
     price: 7.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_99a98f2d-1d64-490d-b202-28fe46134562',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Whether you want to go big or small, this set of 3-Pairs of Hoop Earrings from A New Day&#153; will let you shine with just the right look that you want. Made with stainless steel, brass and nickel-free metal, these beautifully finished hoop earrings are delicately detailed and come in graduated sizes. The classic and simple look makes them perfect to wear for any occasion and with any outfit. Lightweight and easy to care for, these earrings are secured by a snap post closure for easy wear. Pair any of these metal hoop earrings with your favorite outfits to dress up your everyday look.',
     reviews: [
@@ -1746,7 +1746,7 @@ const newJeweleryData = [
     price: 7.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_2aeecefe-d03b-4859-bab1-56d76197053d',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Level up your accessory game with the Flat Geometric Brass and in Worn Gold Post Top Earrings from Universal Thread™. Designed with a worn, textured gold finish, this pair of earrings consists of flattened, inverted U-shape charms with a 1.75-inch drop length that makes for subtle movement. The golden arches hang from the flattened disc earpieces and are fitted with simple post back closures for quick and easy wear. Modern and stylish, this pair will complement a range of outfits from jeans and a tee to a sundress or more formal attire.',
     reviews: [
@@ -1777,7 +1777,7 @@ const newJeweleryData = [
     price: 4.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_ed38f7c3-29f9-48f9-ad45-dc466fbd9858',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       "Sometimes you fall in love with a pair of earrings only to discover that they're sold without backs &mdash; or, worse, you lose one or both of the backs that your favorite pair of earrings came with. Both dilemmas are solved thanks to these Multi Part Rubber Disc Earring Backs from A New Day&#153;. This variety pack of clear earring backs lets you wear your earrings any way you like, comfortably and with the backing style you prefer. Simply select the silver, gold or clear backs that best match your earrings, fasten to the earring posts and confidently flaunt your studs, tassels or hoops.",
     reviews: [
@@ -1808,7 +1808,7 @@ const newJeweleryData = [
     price: 4.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_ce516097-2fee-4372-8aed-7820c7e04b3a',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Connoisseurs Silver Jewelry Cleaner rejuventates dull or tarnished jewelry in just 10 seconds.   It is a fast, convenient way to clean sterling silver jewelry. Use our Silver Jewelry Cleaner to instantly remove tarnish from sterling silver chains, rings, bracelets and earrings and restore its shine and brilliance.  Ammonia-Free. 8 FL. OZ. (236 ml)',
     reviews: [
@@ -1838,7 +1838,7 @@ const newJeweleryData = [
     price: 6,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_54ba5619-6b49-4b07-9811-0b96e24df22c',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Earring set includes three pairs with one-inch drop length and different designs featuring either two-circle or three-circle frame for added movement. Nickel-free metal for comfort with most skin types, and click-top closures lend secure wear on pierced ears.',
     reviews: [
@@ -1859,7 +1859,7 @@ const newJeweleryData = [
     price: 7.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_33daf3db-ab43-4326-ab66-01531c60e588',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Whether you want to go big or small, this set of 3-Pairs of Hoop Earrings from A New Day&#153; will let you shine with just the right look that you want. Made with stainless steel, brass and nickel-free metal, these beautifully finished hoop earrings are delicately detailed and come in graduated sizes. The classic and simple look makes them perfect to wear for any occasion and with any outfit. Lightweight and easy to care for, these earrings are secured by a snap post closure for easy wear. Pair any of these silver hoop earrings with your favorite outfits to dress up your everyday look.',
     reviews: [
@@ -1890,7 +1890,7 @@ const newJeweleryData = [
     price: 10,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_70df085e-bec3-4ce7-b1d6-58e7dbb5c469',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Necklace set includes five necklaces for a layered look. Can be worn together or as standalone pieces, each featuring a different design — ranging from faux pearls to acrylic stones and star-shaped pendants.',
     reviews: [
@@ -1922,7 +1922,7 @@ const newJeweleryData = [
     price: 14.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_6b23589a-c715-4ec2-94b4-3fa1cad26783',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'The Ball & Medallion in Worn Gold Layer Necklace from Universal Thread™ is a beautiful addition to your jewelry collection. This 16-inch gold-finish chain necklace features two chains sitting atop one another for a lovely cascading effect. The upper box chain is accented with golden balls hanging at regular intervals along the entire length, while the lower cable chain is strung with tiny, cylindrical links in a hammered finish and accented with a medallion charm pendant in textured gold tone for a standout look. This double-layered necklace closes with a lobster-claw clasp at the back, where an extra length of chain makes the length fully adjustable.',
     reviews: [
@@ -1951,7 +1951,7 @@ const newJeweleryData = [
     price: 4.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_48e6c010-86d0-4efc-acb5-8883adb0def2',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'The Necklace Chain Extender Set from A New Day&#153; allows for a perfectly customized fit, every time. Lend versatility and variation to your existing jewelry collection by converting your favorite short necklace into a long one. This necklace chain extender set contains two gold and two silver chain extenders in varying lengths to match each of your necklaces.',
     reviews: [
@@ -1982,7 +1982,7 @@ const newJeweleryData = [
     price: 6,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_36ddbf23-6bf7-4009-982e-d35af2c2150c',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Earring set includes three pairs of sparkly hoop earrings, each with the same design in varying sizes. Acrylic crystal rhinestones are embedded in each hoop, finished in silver for a shiny look.',
     reviews: [
@@ -2003,7 +2003,7 @@ const newJeweleryData = [
     price: 6,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_3ba0dbdb-e1e7-43c9-bb1c-672b71ed3d01',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Set of three pairs of medium, slim hoop earrings. Each pair features a different metallic finish — gold, rose gold and silver. Post construction with latch back hold earrings in place for comfortable and secure wear.',
     reviews: [
@@ -2032,7 +2032,7 @@ const newJeweleryData = [
     price: 8.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_96c70b78-d57c-45c0-9a18-14780b207482',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'This elegantly designed jewelry cleaner cleans your jewelry in fresh solution every time. No more cleaning in dirty solution. Just squeeze, dip and dazzle! Non Toxic. Includes Advanced Jewelry Cleaner (enough for over 80 (10 drops) cleanings), Easy to use dipping scoop, now holds more jewelry, easy-grip cleansing brush is specialized to clean all jewelry surfaces and the cleansing container conveniently stores brush, scoop, & drops.',
     reviews: [
@@ -2062,7 +2062,7 @@ const newJeweleryData = [
     price: 6,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_c8416ea0-386b-4dce-aaf8-2beb27766d31',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Searching for that pot ‘o gold? It’s been right under your nose this whole time! Treat your accessories collection to a small fortune with the St. Patrick’s Day Pot of Gold Earrings. These earrings commemorate the ‘ole leprechaun fable of a pot of gold sitting at the end of a rainbow, as faux crystals make up the rainbow and gold in a shimmery black pot with a sparkly shamrock. Wear these earrings with a green headband for a sweet and festive look, or throw them on with your leprechaun ensemble for an ultra-charming costume.',
     reviews: [
@@ -2084,7 +2084,7 @@ const newJeweleryData = [
     price: 4.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_69ce7a2e-8336-4420-b7d4-e30ea6d8da26',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       "For all those times you've lost an earring back, this Earring Back Set has you covered. The set includes four styles of earring backs in multiple colors so you'll always have the right option to secure your earrings &mdash; with 28 earring backs in gold, clear and silver, you'll always have a backup.. JEWELRY DISCLAIMER",
     reviews: [
@@ -2104,7 +2104,7 @@ const newJeweleryData = [
     price: 7.99,
     imageUrl:
       'https://target.scene7.com/is/image/Target/GUEST_b65fd992-cc28-4b98-9d86-32b8a3fd44b2',
-    category: 'jewelery',
+    category: 'jewelry',
     description:
       'Any day can feel a bit more glamorous when you put on the Pave Stone Hoop Earrings from A New Day&#153;. With a metallic finish along the outside, these pave stone earrings are accented with clear stones along the front and on the interior of the hoop, adding an extra dose of shine to everyday looks. Add these pave hoop earrings to a wrap dress and heels for a special date night or wear with a V-neck sweater and jeans for a chic finishing touch to your casual ensemble.',
     reviews: [
@@ -2206,7 +2206,7 @@ async function seed() {
         })
       ),
       await Promise.all(
-        newJeweleryData.map(async product => {
+        newJewelryData.map(async product => {
           let {name, price, imageUrl, category, description, reviews} = product
           const newReviews = await Promise.all(
             reviews.map(review => {
