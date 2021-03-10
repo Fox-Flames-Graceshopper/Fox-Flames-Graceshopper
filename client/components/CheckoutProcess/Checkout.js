710ee1a34a15bd33e48a7d3fde9113ec435e53e6
John Vazquez <johnnyvcoding@gmail.com>
2021-03-10 09:46:05 -0600

    changed copyright

--- client/components/CheckoutProcess/Checkout.js
+++ client/components/CheckoutProcess/Checkout.js
@@ -18,8 +18,11 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link
+        color="inherit"
+        href="https:https://foxflamegraceshopper.herokuapp.com/"
+      >
+        Flameazon
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
